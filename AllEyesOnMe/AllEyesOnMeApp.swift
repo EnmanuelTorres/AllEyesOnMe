fb80d4bb9803a3cd41f0dedaf14790412124920c
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2024-04-18 17:29:03 -0500

    Initial Commit

---
+++ AllEyesOnMe/AllEyesOnMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllEyesOnMeApp.swift
+//  AllEyesOnMe
+//
+//  Created by ENMANUEL TORRES on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AllEyesOnMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
